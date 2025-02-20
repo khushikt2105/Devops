0f4fd492b52f898e304294aa00b92096b96d20af
Khushi Tiwari <97404970+khushikt2105@users.noreply.github.com>
2025-02-20 16:50:46 +0530

    Number guessing Game

--- abc.py
+++ abc.py
@@ -1,1 +1,31 @@
-import numpy as np
+import random
+
+def number_guessing_game():
+    print("Welcome Player....\nThe Game has been started....")
+
+    Min = int(input("Enter the lower value of the range: "))
+    Max = int(input("Enter the upper value of the range: "))
+
+    if Min >= Max:
+        print("Invalid range. The maximum value must be greater than the Minimum value.")
+        return
+
+    number = random.randint(Min, Max)
+    chances = 5
+
+
+    print(f"You have total {chances} chances to guess the correct number to win the game.")
+
+    for attempt in range(1, chances + 1):
+        guess = int(input(f"Attempt {attempt}:\nYour guessed number is: "))
+
+        if guess < number:
+            print("Too low! Try again.")
+        elif guess > number:
+            print("Too high! Try again.")
+        else:
+            print(f"Congratulations! You've guessed the correct which is {number}...")
+            break
+    else:
+        print(f"You have used all your chances.\nThe correct number is {number}...")
+number_guessing_game()
